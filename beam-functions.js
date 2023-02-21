7fa581afcbd316ad72ce73bfc32bec06f736a0c3
Baron Becker <71981227+The-Baron-Becker@users.noreply.github.com>
2023-02-21 16:11:33 -0500

    moved reload again

--- beam-functions.js
+++ beam-functions.js
@@ -315,13 +315,14 @@ async function connectWalletWithMetaMask(e) {
   }
   alert(`Your wallet: ${walletAddress} has now been connected!`);
   loadUserWallet();
+  location.reload();
 
 }
 
 connectWalletBtn.addEventListener("click", connectWalletWithMetaMask);
 loadUserWallet();
 checkIfMembershipNeedsPayment();
-location.reload();
+
 
 // Register METABEAM
 $(".register-metabeam-btn").click(async (e) => {
