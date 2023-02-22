105590bcc71482448d99095276fc29d792b1acbd
BaronBecker <60110113+BaronBecker@users.noreply.github.com>
2023-02-22 17:24:51 -0500

    Revert "typo"
    This reverts commit 2908a841602a6ef8c87502d3769f9b07b477e122.

--- beam-functions.js
+++ beam-functions.js
@@ -331,7 +331,7 @@ $(".register-metabeam-btn").click(async (e) => {
   const user_code = encodeURIComponent($(".registration-code").val());
   const walletAddress = window.getCookie("user wallet address")
   const res = await window.makeAPIRequestWithToken(
-    `pairing/consume?wallet_address=${walletAddress}&user_code=${user_code}`
+    `pairing/consume??wallet_address=${walletAddress}&user_code=${user_code}`
   );
   $(".register-modal-div").hide();
   loadUserDevicesList();
