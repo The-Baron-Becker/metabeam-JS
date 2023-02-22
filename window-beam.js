557ca27a841f5d5a093865ee7e7d375bb03da39a
BaronBecker <60110113+BaronBecker@users.noreply.github.com>
2023-02-22 17:24:49 -0500

    Revert "swapped token with user wallet address"
    This reverts commit e61adee02ec94be355ff408e531ba166145a0e2a.

--- window-beam.js
+++ window-beam.js
@@ -21,7 +21,7 @@ window.getCookie = (cname) => {
 };
 window.baseUrl = "https://nftviewr.completewebtech.com";
 window.makeAPIRequestWithToken = async (endpoint, options = {}) => {
-  const token = window.getCookie("user wallet address");
+  const token = window.getCookie("token");
   if (token === "") {
     document.querySelector("#sign-in-modal").style.display = "block";
     return { status: "Not logged in" };
