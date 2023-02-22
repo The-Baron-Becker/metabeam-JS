d353154ba2fec5407119058aa8449ed527259ab7
Baron Becker <71981227+The-Baron-Becker@users.noreply.github.com>
2023-02-21 20:39:51 -0500

    modified register code to use wallet address

--- beam-functions.js
+++ beam-functions.js
@@ -329,8 +329,9 @@ $(".register-metabeam-btn").click(async (e) => {
   e.preventDefault();
   const name = encodeURIComponent($(".metabeam-name").val());
   const user_code = encodeURIComponent($(".registration-code").val());
+  const walletAddress = window.getCookie("user wallet address")
   const res = await window.makeAPIRequestWithToken(
-    `pairing/consume?name=${name}&user_code=${user_code}`
+    `pairing/consume??wallet_address=${walletAddress}&user_code=${user_code}`
   );
   $(".register-modal-div").hide();
   loadUserDevicesList();
