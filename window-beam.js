cd05e5024726228e3853e32f8a691486bee5d459
BaronBecker <60110113+BaronBecker@users.noreply.github.com>
2023-02-22 17:24:18 -0500

    Revert "added question mark"
    This reverts commit 6a83f2411fba34d58b5933ec97ae7e655434c303.

--- window-beam.js
+++ window-beam.js
@@ -41,7 +41,7 @@ window.baseUrl = "https://nftviewr.completewebtech.com";
 window.makeAPIRequestForRegister = async (endpoint, options = {}) => {
   const token = window.getCookie("user wallet address");
   try {
-    const res = await fetch(`${baseUrl}/?${endpoint}&wallet_address=${token}`, {
+    const res = await fetch(`${baseUrl}/${endpoint}&wallet_address=${token}`, {
       headers: {
         "Content-Type": "application/json",
         "x-access-tokens": token,
