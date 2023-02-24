471420ad3c47dae499e5255b8b0635d05418dc75
Baron Becker <71981227+The-Baron-Becker@users.noreply.github.com>
2023-02-24 00:21:37 -0500

    new place

--- beam-functions.js
+++ beam-functions.js
@@ -366,6 +366,8 @@ async function connectWalletWithMetaMask(e) {
   alert(`Your wallet: ${walletAddress} has now been connected!`);
   loadUserWallet();
   location.reload();
+  loadUserDevicesList();
+
 }
 
 $connectWalletBtn.click(connectWalletWithMetaMask);
