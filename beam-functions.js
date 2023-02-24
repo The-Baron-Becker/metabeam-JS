545936d8c6d57c8311aff56e3767d933ec88c4ca
BaronBecker <60110113+BaronBecker@users.noreply.github.com>
2023-02-23 21:40:01 -0500

    Revert "attempting to fix #gallery filter"
    This reverts commit f6230cd44daad41623f678cb525d0e5c71474971.

--- beam-functions.js
+++ beam-functions.js
@@ -75,7 +75,7 @@ function swapOutLightbox(nftSrc, name, data, isVideoFile) {
     );
   }, 500);
 }
-makeAPIRequest(`search?q=#gallery`).then((nfts) => {
+makeAPIRequest(`random?size=20`).then((nfts) => {
   const walletAddress = window.getCookie("user wallet address");
   const $galleryItems = $(`.new-gallery-grid img`);
   const $galleryItemsTitles = $(`.new-gallery-grid .nft-title`);
