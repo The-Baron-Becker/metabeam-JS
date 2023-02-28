55c1a4fb2e08f85aeff359aacac8729070a59be3
BaronBecker <60110113+BaronBecker@users.noreply.github.com>
2023-02-28 13:41:07 -0500

    invalid creds message

--- beam-functions.js
+++ beam-functions.js
@@ -338,10 +338,14 @@ async function login(e, register = false) {
       password,
     }),
   });
-  window.setCookie("token", res.token);
-  $("#sign-in-modal").hide();
-///  checkIfMembershipNeedsPayment();
-///  window.initializeStripe();
+  
+  if (res.token) {
+    window.setCookie("token", res.token);
+    $("#sign-in-modal").hide();
+  } else {
+    const invalidCredentialsDiv = document.querySelector(".invalid-credentials");
+    invalidCredentialsDiv.style.display = "block";
+  }
 }
 
 $registerBtn.click((e) => login(e, true));
